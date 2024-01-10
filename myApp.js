d79dea19d4844eeb0f63c5a20efe7bfb0adff232
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-01-10 17:36:05 -0500

    feat: add time server

--- myApp.js
+++ myApp.js
@@ -21,6 +21,13 @@ app.get("/json",function(req,res){
     res.json({"message": `${process.env.MESSAGE_STYLE==='uppercase' ? 'HELLO JSON' : 'Hello json'}`})
 })
 
+// Chain Middleware to Create a Time Server
+app.get('/now', function(req,res,next){
+    req.time = new Date().toString()
+    next();
+},function(req,res){
+    res.send({time: req.time})
+})
 
 
 
