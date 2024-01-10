e0b58f3509920e90349d1b99823fbebbb79aad19
kylestech95 <kylestech95@outlook.com>
2024-01-10 09:54:49 -0500

    Initial commit

--- myApp.js
+++ myApp.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
+// app.get("/",function(req,res){
+//     res.send('Hello Express')
+// })
+app.get("/",function(req,res){
+    res.sendFile(__dirname + '/views/index.html')
+})
+// middleware
+app.use("/public",express.static(__dirname + '/public'))
 
-
+// rest API
+app.get("/json",function(req,res){
+    res.json({"message": `${process.env.MESSAGE_STYLE==='uppercase' ? 'HELLO JSON' : 'Hello json'}`})
+})
 
 
 
