337191bc24d8dfbb82ba7ad4347276e7c9cc1a74
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2024-01-10 16:26:56 -0500

    feat: add middleware logger

--- myApp.js
+++ myApp.js
@@ -6,7 +6,13 @@ let app = express();
 // })
 app.get("/",function(req,res){
     res.sendFile(__dirname + '/views/index.html')
+    //root middleware (logger)
 })
+app.use('/',function(req,res,next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+     next();
+})
+
 // middleware
 app.use("/public",express.static(__dirname + '/public'))
 
@@ -14,8 +20,6 @@ app.use("/public",express.static(__dirname + '/public'))
 app.get("/json",function(req,res){
     res.json({"message": `${process.env.MESSAGE_STYLE==='uppercase' ? 'HELLO JSON' : 'Hello json'}`})
 })
-
-
 
 
 
